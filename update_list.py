0a0fe55074f68fce98ae00933858a4d4cd4daa7f
batuhamsabri55 <135415882+batuhansabri55@users.noreply.github.com>
2026-03-29 21:00:36 +0300

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -11,6 +11,7 @@ YEDEK_KAYNAKLAR = [
     "https://streams.uzunmuhalefet.com/lists/tr.m3u",
     "https://tinyurl.com/ytpatron",
     "https://urlz.fr/v1Xo",
+    "https://raw.githubusercontent.com/smartgmr/cdn/refs/heads/main/Perfect.m3u",
     "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/tr.m3u",
     "https://raw.githubusercontent.com/yasarfalkan/m3u-dosyam/refs/heads/main/YMBK.m3u8",
     "https://tinyurl.com/bdd2tz6h",
