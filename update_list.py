f586b8761a6c33fbea3323a662ce24a372d81821
batuhamsabri55 <135415882+batuhansabri55@users.noreply.github.com>
2026-04-10 02:38:15 +0300

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -5,7 +5,7 @@ import datetime
 
 # --- AYARLAR ---
 FILE_PATH = "tr.m3u"
-# USTA, İLK 3950 SATIR DOKUNULMAZDIR. YENİLER BUNUN SONUNA EKLENİR.
+# USTA, İLK 4050 SATIR DOKUNULMAZDIR. YENİLER BUNUN SONUNA EKLENİR.
 ZIRH_LIMIT = 4050
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
 
