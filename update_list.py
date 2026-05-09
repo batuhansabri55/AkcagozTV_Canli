227f623d8fd8ba502b946965b72df22bc76d1d77
batuhamsabri55 <135415882+batuhansabri55@users.noreply.github.com>
2026-05-09 18:20:22 +0300

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -36,7 +36,6 @@ YEDEK_KAYNAKLAR = [
     "https://raw.githubusercontent.com/smartgmr/cdn/refs/heads/main/Perfect.m3u",
     "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/tr.m3u",
     "https://raw.githubusercontent.com/yasarfalkan/m3u-dosyam/refs/heads/main/YMBK.m3u8",
-    "https://files.manuscdn.com/user_upload_by_module/session_file/310519663091167371/lXQCJEWGepXILedX.m3u8",
     "https://tinyurl.com/bdd2tz6h",
     "https://publiciptv.com/countries/tr/m3u",
     "https://iptv-org.github.io/iptv/countries/tr.m3u"
