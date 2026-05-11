870643a0143e5f61c3ce008e6c06742de898f73b
batuhamsabri55 <135415882+batuhansabri55@users.noreply.github.com>
2026-05-11 23:51:10 +0300

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -12,7 +12,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # --- AYARLAR ---
 FILE_PATH = "tr.m3u"
-ZIRH_LIMIT = 3900
+ZIRH_LIMIT = 3950
 THREADS = 4        
 
 HEADERS = {
