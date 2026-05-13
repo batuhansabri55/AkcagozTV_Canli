50d4a744b2bd4cb7c7bd2183fbe62541099251d3
batuhamsabri55 <135415882+batuhansabri55@users.noreply.github.com>
2026-05-13 22:34:13 +0300

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -37,7 +37,6 @@ YEDEK_KAYNAKLAR = [
     "https://raw.githubusercontent.com/yasarfalkan/m3u-dosyam/refs/heads/main/YMBK.m3u8",
     "https://tinyurl.com/bdd2tz6h",
     "https://publiciptv.com/countries/tr/m3u",
-    "view-source:https://publiciptv.com/countries/tr/m3u",
     "https://iptv-org.github.io/iptv/countries/tr.m3u"
 ]
 
