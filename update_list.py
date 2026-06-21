bef6c9d554e823dd75e0d7488adb7348062c3b0a
batuhamsabri55 <135415882+batuhansabri55@users.noreply.github.com>
2026-06-21 10:31:19 +0300

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -37,6 +37,7 @@ YASAKLI_IP_LISTESI = [
 YEDEK_KAYNAKLAR = [
     "https://streams.uzunmuhalefet.com/lists/tr.m3u",
     "https://raw.githubusercontent.com/hayatiptv/iptv/master/index.m3u",
+    "https://link.testworkery0.workers.dev/patron.m3u",
     "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/tr.m3u",
     "https://raw.githubusercontent.com/yasarfalkan/m3u-dosyam/refs/heads/main/YMBK.m3u8",
     "https://www.dropbox.com/scl/fi/p58t5o980tah2hz3234a5/SmartGO.m3u?rlkey=w44w0ycaa83uyn21uph77pp6v&st=mj0n6byr&raw=1",
