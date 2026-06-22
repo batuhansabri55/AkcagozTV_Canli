15e905c6830f687c5830195ce623ef04bf447563
batuhamsabri55 <135415882+batuhansabri55@users.noreply.github.com>
2026-06-23 00:33:01 +0300

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -13,7 +13,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # --- AYARLAR ---
 FILE_PATH = "tr.m3u"
-ZIRH_LIMIT = 4000
+ZIRH_LIMIT = 4200
 THREADS = 64         # 5600 link için tam güç hız ayarı!
 
 HEADERS = {
